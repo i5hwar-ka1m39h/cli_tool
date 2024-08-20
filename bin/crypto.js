ca48689955837e558ac9a8245e36888c8d4f3de8
ishwar kalmegh <ishwarkalmegh156@gmail.com>
2024-08-20 16:52:36 +0530

    the get 5 coin functionality added

--- bin/crypto.js
+++ bin/crypto.js
@@ -4,17 +4,36 @@
 const {program} = require('commander');
 const pkg = require('../package.json');
 const { default: axios } = require('axios');
+const colors = require('colors')
 
 program.version(pkg.version)
 
-const main = async() =>{
+
+
+const getTop = async() =>{
     const response = await axios.get("https://api.coincap.io/v2/assets")
-    console.log(response.data.data[0]);
+    let data = response.data.data;
+
+    console.log("Top 5 Cryptocurrencies:");
+    console.log("--------------------------------------------------");
+    console.log("Name".padEnd(15) + "Symbol".padEnd(10) + "Price (USD)");
+    console.log("--------------------------------------------------");
+
+    data.slice(0,5).map(each=>{
+        const name =colors.green( each.name.padEnd(15))
+        const symbol = colors.blue( each.symbol.padEnd(10))
+        const price = colors.cyan( each.priceUsd)
+        console.log(`${name}${symbol}${price}`);
+        
+    })
+
+    console.log("--------------------------------------------------");
+
     
     
 }
 
-program.command('set').description('sets the command for something').action(async()=>main())
+program.command('get').description('gets the top 5 cryptocurrency infomation').action(async()=>getTop())
 
 
 
