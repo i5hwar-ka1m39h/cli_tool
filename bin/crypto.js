b6af663c180ccd153d95bcd358ce0c2922055ccc
ishwar kalmegh <ishwarkalmegh156@gmail.com>
2024-08-21 23:33:27 +0530

    added new feature but nothing extraordinary

--- bin/crypto.js
+++ bin/crypto.js
@@ -12,51 +12,87 @@ program.version(pkg.version)
 
 
 const getTop = async() =>{
-    const response = await axios.get("https://api.coincap.io/v2/assets")
-    let data = response.data.data;
-
-    console.log("Top 5 Cryptocurrencies:");
-    console.log("--------------------------------------------------");
-    console.log("Name".padEnd(15) + "Symbol".padEnd(10) + "Price (USD)");
-    console.log("--------------------------------------------------");
-
-    data.slice(0,5).map(each=>{
-        const name =colors.green( each.name.padEnd(15))
-        const symbol = colors.blue( each.symbol.padEnd(10))
-        const price = colors.cyan( each.priceUsd)
-        console.log(`${name}${symbol}${price}`);
+    try {
+        const response = await axios.get("https://api.coincap.io/v2/assets")
+        let data = response.data.data;
+    
+        console.log("Top 5 Cryptocurrencies:");
+        console.log("--------------------------------------------------");
+        console.log("Name".padEnd(15) + "Symbol".padEnd(10) + "Price (USD)");
+        console.log("--------------------------------------------------");
+    
+        data.slice(0,5).map(each=>{
+            const name =colors.green( each.name.padEnd(15))
+            const symbol = colors.blue( each.symbol.padEnd(10))
+            const price = colors.cyan( each.priceUsd)
+            console.log(`${name}${symbol}${price}`);
+            
+        })
+    
+        console.log("--------------------------------------------------");   
         
-    })
-
-    console.log("--------------------------------------------------");
-
-    
-    
+    } catch (error) {
+        console.error(error);
+        
+    } 
 }
 
 const getCoin = async(coin) =>{
-    const coinId = coinData[coin.toUpperCase()]
 
-    const response = await axios.get(`https://api.coincap.io/v2/assets/${coinId}`)
-    let data = response.data.data;
+    try {
+        const coinId = coinData[coin.toUpperCase()]
 
-    console.log("--------------------------------------------------");
-    console.log(`${coinId} `.yellow + 'info');
-    console.log("--------------------------------------------------");
-
-    console.log('symbol:'.padEnd(15)+`${data.symbol}`.blue + '\n' +
-        'rank:'.padEnd(15) + `${data.rank}`.red + '\n' +
-        'price:'.padEnd(15) + `$ `.green+ `${data.priceUsd}` + '\n' 
-
-
-
-    );
-
+        const response = await axios.get(`https://api.coincap.io/v2/assets/${coinId}`)
+        let data = response.data.data;
     
+        console.log("--------------------------------------------------");
+        console.log(`${coinId} `.yellow + 'info');
+        console.log("--------------------------------------------------");
     
-    
-    
+        console.log('symbol:'.padEnd(15)+`${data.symbol}`.blue + '\n' +
+            'rank:'.padEnd(15) + `${data.rank}`.red + '\n' +
+            'price:'.padEnd(15) + `$ `.green+ `${data.priceUsd}` + '\n' +
+            'market supply:'.padEnd(15) + `${data.supply}`.cyan + '\n' +
+            'max supply:'.padEnd(15) + `${data.maxSupply}`.cyan + '\n' +
+            'market cap:'.padEnd(15) + `$ `.green + `${data.marketCapUsd}`.cyan + '\n'
+        ); 
+
+    } catch (error) {
+        console.error(error);
+    }
+   
 }
+
+const getCoins = async() => {
+    try {
+        const response = await axios.get("https://api.coincap.io/v2/assets")
+        const data  = response.data.data
+    
+        let coinMap = new Map()
+        data.map(coin=>{
+            coinMap.set(coin.name, coin.rank)
+        })
+    
+        console.log(coinMap);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const getSym = async() =>{
+    try {
+        let key = Object.keys(coinData);
+        let value = Object.values(coinData)
+
+        for(let i = 0; i < key.length; i++){
+            console.log(`${value[i]}`.green + `:` + `${key[i]}`.cyan);
+            
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 
 program
     .command('get')
@@ -72,6 +108,12 @@ program
 
 
 
+program
+.command('coins')
+.description('get all coins in the database ')
+.action(async()=>await getCoins())
+
+program.command('symbol').description('get all the coin symbol').action(async()=>await getSym())
 
 
 
